27fa9ce612c0a2ec7363c47e2b4d71ba1dd89fc9
Jjvv21 <47864787+Jjvv21@users.noreply.github.com>
2020-06-14 21:50:11 -0600

    Piedra Papel o Tijera Completo

---
+++ src/Juego.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Juego extends Application{
+
+	PiedraPapelOtijera interfaz = new PiedraPapelOtijera();
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Scene scene = new Scene(interfaz.getPane(), 400, 400);
+		primaryStage.setTitle("PiedraPapeloTijera");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	} 
+    public static void main(String[] args) {
+    	launch(args);
+    
+    }
+}
